d1012003b4efab61c7cc7f650f3ee7afdad5d448
Andy Ouyang <andy.ouyang@stonybrook.edu>
2019-03-25 21:22:21 -0400

    nvm

--- __init__.py
+++ __init__.py
@@ -17,13 +17,13 @@ class Authenticate(Resource):
 		print('--------------------------args:' + str(args), sys.stderr)
 		user = users.find_one({'username': args['username']})
 		if user is None:
-			return {'status': 'error', 'error': 'invalid username'}
+			return {'status': 'error', 'message': 'invalid username'}
 		elif user['password'] != args['password']:
-			return {'status': 'error', 'error': 'incorrect password'}
+			return {'status': 'error', 'message': 'incorrect password'}
 		elif user['enabled']:
 			return {'status': 'OK'}
 		else:
-			return {'status': 'error', 'error': 'not verified'}
+			return {'status': 'error', 'message': 'not verified'}
 
 class Verify(Resource):
 	def post(self):
